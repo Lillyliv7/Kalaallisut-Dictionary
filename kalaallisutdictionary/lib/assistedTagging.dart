36a821fb2ec621477cce192d940d7ab2892c24c9
Lillyliv7 <levilive1245@gmail.com>
2026-04-15 12:24:49 -0400

    i forgor

---
+++ kalaallisutdictionary/lib/assistedTagging.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class taggingPage extends StatefulWidget {
+  const taggingPage({super.key});
+
+  @override
+  State<taggingPage> createState() => _taggingPageState();
+}
+
+class _taggingPageState extends State<taggingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
